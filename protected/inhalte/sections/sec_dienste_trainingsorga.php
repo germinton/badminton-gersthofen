7ea9a7ecbd971168b2a8a076b69a2142111b5c5c
germinton <germinton@googlemail.com>
2016-02-03 22:03:56 +0100

    sync with ftp server
    sync with ftp server (see also Issue #16)

---
+++ protected/inhalte/sections/sec_dienste_trainingsorga.php
@@ -0,0 +1,7 @@
+<h1>Trainingsorganisation</h1>
+<h2>Trainer-Doodle:</h2>
+<p><a href="http://doodle.com/poll/by5myu4sh5hk8ee8#admin">http://doodle.com/poll/by5myu4sh5hk8ee8#admin</a></p>
+<h2>Hallenequipment-Doodle:</h2>
+<p><a href="http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table">http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table</a></p>
+<h2>Helfer-Doodle:</h2>
+<p><a href="http://doodle.com/poll/ugzx9ia7ukrkf9mh">http://doodle.com/poll/ugzx9ia7ukrkf9mh</a></p>
