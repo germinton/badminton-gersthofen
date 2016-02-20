fdf0db5fb4147221ab2abf900b936482274cd7f1
Daniel Toplak <homer.simpson123@gmx.de>
2016-02-20 10:30:16 +0100

    #noissue: Update Trainer Doodle Link

--- protected/inhalte/sections/sec_dienste_trainingsorga.php
+++ protected/inhalte/sections/sec_dienste_trainingsorga.php
@@ -1,6 +1,6 @@
 <h1>Trainingsorganisation</h1>
 <h2>Trainer-Doodle:</h2>
-<p><a href="http://doodle.com/poll/by5myu4sh5hk8ee8#admin">http://doodle.com/poll/by5myu4sh5hk8ee8#admin</a></p>
+<p><a href="http://doodle.com/poll/e7kv734pgxaiwzfm">http://doodle.com/poll/e7kv734pgxaiwzfm</a></p>
 <h2>Hallenequipment-Doodle:</h2>
 <p><a href="http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table">http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table</a></p>
 <h2>Helfer-Doodle:</h2>
