a31192bced4bdcfba128af7a30d5540e48ef3bbd
Daniel Toplak <homer.simpson123@gmx.de>
2016-03-02 01:16:23 +0100

    #noissue: fix: Helfer Doodle entfernt -> ist jetzt mit im Trainer-Orga-Doodle

--- protected/inhalte/sections/sec_dienste_trainingsorga.php
+++ protected/inhalte/sections/sec_dienste_trainingsorga.php
@@ -1,7 +1,5 @@
 <h1>Trainingsorganisation</h1>
-<h2>Trainer-Doodle:</h2>
+<h2>Trainer-/Helferdoodle Schüler/Jugend/Aktive:</h2>
 <p><a href="http://doodle.com/poll/e7kv734pgxaiwzfm">http://doodle.com/poll/e7kv734pgxaiwzfm</a></p>
 <h2>Hallenequipment-Doodle:</h2>
-<p><a href="http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table">http://doodle.com/poll/3t9av8hkvvzck9w28cwu2inm/admin#table</a></p>
-<h2>Helfer-Doodle:</h2>
-<p><a href="http://doodle.com/poll/ugzx9ia7ukrkf9mh">http://doodle.com/poll/ugzx9ia7ukrkf9mh</a></p>
+<p><a href="http://doodle.com/poll/3t9av8hkvvzck9w2">http://doodle.com/poll/3t9av8hkvvzck9w2</a></p>
