2cbc373808ec6cd8cfdf2d36ed9e26138047258a
Daniel Toplak <homer.simpson123@gmx.de>
2016-03-21 23:04:44 +0100

    #noissue: Trainingszeiten und Orte geändert

--- protected/inhalte/sections/sec_training_jugend.php
+++ protected/inhalte/sections/sec_training_jugend.php
@@ -19,14 +19,18 @@ für Ihre Mannschaft.</p>
 	<tr>
 		<th>Freitag</th>
 		<td>
-		<p>16-17:30Uhr<br />
-		<a href="index.php?section=sportstaetten#austragungsort_id:3">PKG-Halle</a></p></td>
+			<p>17:30-19:00 Uhr<br />
+				<a href="index.php?section=sportstaetten#austragungsort_id:278">Mittelschulhalle (neu)</a>
+			</p>
+		</td>
 	</tr>
 	<tr>
 		<th>Sonntag</th>
 		<td>
-		<p>18-19:30Uhr<br />
-		<a href="index.php?section=sportstaetten#austragungsort_id:278">Mittelschulhalle (neu)</a></p></td>
+			<p>18:00-19:30 Uhr<br />
+				<a href="index.php?section=sportstaetten#austragungsort_id:278">Mittelschulhalle (neu)</a>
+			</p>
+		</td>
 	</tr>
 </table>
 
