ece991e4888fc6752e7d0229f96b35abf39d6c89
SonicFab <f.golembiewski@gmx.net>
2017-03-14 19:51:10 +0100

    Gebührenseite: Rechtschreibfehler behoben und Kosten aktualisiert

--- protected/inhalte/sections/sec_gebuehren.php
+++ protected/inhalte/sections/sec_gebuehren.php
@@ -9,7 +9,7 @@ und dem <strong>Abteilungsbeitrag</strong> zusammen. Beide Beträge werden per L
 Trainer und Hallenmiete und berechtigt zur Teilnahme an Übungsstunden aller
 Abteilungen. Dieser Beitrag wird halbjährig durch die Verwaltung des Hauptvereins eingezogen.</p>
 
-<p><strong>Abteilungsbeitrag:</strong> Der Abteilungsbeitrag fließt der Abteilung Badminton zu und wird ebenfalls von der Verwaltung des Hauptverein eingezogen. Wir spielen mit Natur- Federbällen aus Gänsefedern. Um einen reibungslosen Trainingsbetrieb sicherzustellen, werden die Federbälle zentral von der Abteilung Badminton besorgt und im Training zur Verfügung gestellt. Von den Qualitäts- und Kostenvorteilen profitieren alle Sportler. Aus diesem Grund erhebt die Abteilung Badminton zusätzlich einen altersabhängigen Abteilungsbeitrag.</p>
+<p><strong>Abteilungsbeitrag:</strong> Der Abteilungsbeitrag fließt der Abteilung Badminton zu und wird ebenfalls von der Verwaltung des Hauptvereins eingezogen. Wir spielen mit Natur- Federbällen aus Gänsefedern. Um einen reibungslosen Trainingsbetrieb sicherzustellen, werden die Federbälle zentral von der Abteilung Badminton besorgt und im Training zur Verfügung gestellt. Von den Qualitäts- und Kostenvorteilen profitieren alle Sportler. Aus diesem Grund erhebt die Abteilung Badminton zusätzlich einen altersabhängigen Abteilungsbeitrag.</p>
 
 <p>&nbsp;</p>
 <table class="schattiert">
@@ -20,14 +20,14 @@ Abteilungen. Dieser Beitrag wird halbjährig durch die Verwaltung des Hauptverei
 	</tr>
 	<tr>
 		<td>Jahres-Mitgliedsbeitrag des TSV Gersthofen</td>
-		<td>84,00 € bis 110,40 €</td>
+		<td>96,00 € bis 126,00 €</td>
 		<td>Beitrag ist abhängig vom Alter<br>
 		(<a href="http://tsv-gersthofen.de/gesch%C3%A4ftsstelle/hv-beitraege.html">Beitrag & Aufnahmeantrag</a>)
 		</td>
 	</tr>
 	<tr>
 		<td>Jahres-Abteilungsbeitrag der Abteilung Badminton</td>
-		<td>54,00 € bis 84,00 €</td>
+		<td>54,00 € bis 108,00 €</td>
 		<td>Hobby & <18 Jahre: 4,50 €<br>
 		Mannschaftsspieler: 7,00 €/Monat<br>
 		(Abbuchung im Januar/Juli, anteilig bei unterjährigem Eintritt)
@@ -36,4 +36,4 @@ Abteilungen. Dieser Beitrag wird halbjährig durch die Verwaltung des Hauptverei
 </table>
 
 <p class="textbox schattiert">Damit ergeben sich in Summe jährlich günstige <em>Gesamtkosten</em> für Training und Bälle <em>in
-Höhe von 138,00 € bis 194,40 €</em>.</p>
+Höhe von 150,00 € bis 234,00 €</em>.</p>
