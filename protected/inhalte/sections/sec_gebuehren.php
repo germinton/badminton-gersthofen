8d9f5c79aca695e917c97d2c75cc41a591269cf4
SonicFab <f.golembiewski@gmx.net>
2017-03-19 09:38:58 +0100

    Anpassungen der Gebühren, für eine bessere Außenwahrnehmung in Bezug auf den Verein (mit Sandro abgeklärt).

--- protected/inhalte/sections/sec_gebuehren.php
+++ protected/inhalte/sections/sec_gebuehren.php
@@ -2,14 +2,17 @@
 
 <p><h2>Was kostet mich Badminton spielen im TSV Gersthofen?</h2></p>
 
-<p>Die Gesamtkosten setzen sich aus dem <strong>Mitgliedsbeitrag für den Hauptverein</strong>
-und dem <strong>Abteilungsbeitrag</strong> zusammen. Beide Beträge werden per Lastschrift eingezogen.</p>
+<p>Die Gesamtkosten setzen sich aus dem <strong>Mitgliedsbeitrag des Vereins</strong>
+und dem <strong>Beitrag der Abteilung</strong> zusammen. Beide Beträge werden per Lastschrift eingezogen.</p>
 
-<p><strong>Mitgliedsbeitrag für den Hauptverein:</strong> Der TSV-Vereinsbeitrag deckt die Verwaltung,
+<p><strong>Mitgliedsbeitrag:</strong> Dieser Beitrag deckt die Verwaltung,
 Trainer und Hallenmiete und berechtigt zur Teilnahme an Übungsstunden aller
-Abteilungen. Dieser Beitrag wird halbjährig durch die Verwaltung des Hauptvereins eingezogen.</p>
+Abteilungen. Dieser Beitrag wird halbjährig durch den Verein eingezogen.</p>
 
-<p><strong>Abteilungsbeitrag:</strong> Der Abteilungsbeitrag fließt der Abteilung Badminton zu und wird ebenfalls von der Verwaltung des Hauptvereins eingezogen. Wir spielen mit Natur- Federbällen aus Gänsefedern. Um einen reibungslosen Trainingsbetrieb sicherzustellen, werden die Federbälle zentral von der Abteilung Badminton besorgt und im Training zur Verfügung gestellt. Von den Qualitäts- und Kostenvorteilen profitieren alle Sportler. Aus diesem Grund erhebt die Abteilung Badminton zusätzlich einen altersabhängigen Abteilungsbeitrag.</p>
+<p><strong>Abteilungsbeitrag:</strong> Dieser Beitrag fließt der Abteilung Badminton zu. Er wird vom Verein zusammen mit dem Mitgliedsbeitrag eingezogen.
+Wir spielen mit Natur- Federbällen aus Gänsefedern. Um einen reibungslosen Trainingsbetrieb sicherzustellen, werden die Federbälle zentral von der Abteilung Badminton
+besorgt und im Training zur Verfügung gestellt. Von den Qualitäts- und Kostenvorteilen profitieren alle Sportler. Aus diesem Grund erhebt die Abteilung Badminton
+zusätzlich einen altersabhängigen Abteilungsbeitrag.</p>
 
 <p>&nbsp;</p>
 <table class="schattiert">
@@ -20,20 +23,20 @@ Abteilungen. Dieser Beitrag wird halbjährig durch die Verwaltung des Hauptverei
 	</tr>
 	<tr>
 		<td>Jahres-Mitgliedsbeitrag des TSV Gersthofen</td>
-		<td>96,00 € bis 126,00 €</td>
+		<td style="white-space:nowrap">96 € bis 126 €</td>
 		<td>Beitrag ist abhängig vom Alter<br>
 		(<a href="http://tsv-gersthofen.de/gesch%C3%A4ftsstelle/hv-beitraege.html">Beitrag & Aufnahmeantrag</a>)
 		</td>
 	</tr>
 	<tr>
 		<td>Jahres-Abteilungsbeitrag der Abteilung Badminton</td>
-		<td>54,00 € bis 108,00 €</td>
+		<td style="white-space:nowrap">54 € bis 108 €</td>
 		<td>Hobby & <18 Jahre: 4,50 €<br>
-		Mannschaftsspieler: 7,00 €/Monat<br>
-		(Abbuchung im Januar/Juli, anteilig bei unterjährigem Eintritt)
+		Mannschaftsspieler: 9 €/Monat<br>
+		(Abbuchung im Februar/Juli, anteilig bei unterjährigem Eintritt)
 		</td>
 	</tr>
 </table>
 
 <p class="textbox schattiert">Damit ergeben sich in Summe jährlich günstige <em>Gesamtkosten</em> für Training und Bälle <em>in
-Höhe von 150,00 € bis 234,00 €</em>.</p>
+Höhe von 150 € bis 234 €</em>.</p>
